6803b653ffcee94886edfb8534fc17438b46f7e3
André Fortini <124889918+andrefrtn@users.noreply.github.com>
2026-05-20 17:40:11 -0300

    botao de logout

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -8,87 +8,101 @@ import { Link, useNavigate, useLocation  } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 export default function Header() {
 
-const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
+  const navigate = useNavigate()
+  const token = localStorage.getItem("token")
 
-const navigate = useNavigate()
+      function logout() {
+      localStorage.removeItem("token")
 
-function logout() {
-  localStorage.removeItem("token")
+      navigate("/")
+    }
 
-  navigate("/")
-}
+      const location = useLocation()
 
-const location = useLocation()
+      useEffect(() => {
+        setMenuOpen(false)
+      }, [location])
 
-useEffect(() => {
-  setMenuOpen(false)
-}, [location])
+
+
 
   return (
- <div className='header'>
-<Link to="/dashboard" className='logo'>
-  Vence<span className='hj'>Hoje</span>
-</Link>
+        <div className='header'>
+        <Link to="/dashboard" className='logo'>
+          Vence<span className='hj'>Hoje</span>
+        </Link>
 
 
     <div className='subHeader'>
-
-<Link to="/dashboard"  className='pages'><svg className='casa' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="30" height="30">
-<path fill="currentColor" d="M277.8 8.6c-12.3-11.4-31.3-11.4-43.5 0l-224 208c-9.6 9-12.8 22.9-8 35.1S18.8 272 32 272l16 0 0 176c0 35.3 28.7 64 64 64l288 0c35.3 0 64-28.7 64-64l0-176 16 0c13.2 0 25-8.1 29.8-20.3s1.6-26.2-8-35.1l-224-208zM240 320l32 0c26.5 0 48 21.5 48 48l0 96-128 0 0-96c0-26.5 21.5-48 48-48z" /></svg>
-DashBoard
-</Link > 
-
-<Link  className='pages'><svg className='din' xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 512 512" width="30" height="30"
-><path fill="currentColor" d="M112 112c0 35.3-28.7 64-64 64l0 160c35.3 0 64 28.7 64 64l288 0c0-35.3 28.7-64 64-64l0-160c-35.3 0-64-28.7-64-64l-288 0zM0 128C0 92.7 28.7 64 64 64l384 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 448c-35.3 0-64-28.7-64-64L0 128zm256 16a112 112 0 1 1 0 224 112 112 0 1 1 0-224zm-16 44c-11 0-20 9-20 20 0 9.7 6.9 17.7 16 19.6l0 48.4-4 0c-11 0-20 9-20 20s9 20 20 20l48 0c11 0 20-9 20-20s-9-20-20-20l-4 0 0-68c0-11-9-20-20-20l-16 0z"/>
-</svg>Contas</Link >
+    <Link to="/dashboard"  className='pages'><svg className='casa' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="30" height="30">
+    <path fill="currentColor" d="M277.8 8.6c-12.3-11.4-31.3-11.4-43.5 0l-224 208c-9.6 9-12.8 22.9-8 35.1S18.8 272 32 272l16 0 0 176c0 35.3 28.7 64 64 64l288 0c35.3 0 64-28.7 64-64l0-176 16 0c13.2 0 25-8.1 29.8-20.3s1.6-26.2-8-35.1l-224-208zM240 320l32 0c26.5 0 48 21.5 48 48l0 96-128 0 0-96c0-26.5 21.5-48 48-48z" /></svg>
+    DashBoard
+    </Link > 
 
 
+    <Link  className='pages'><svg className='din' xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 512 512" width="30" height="30"
+    ><path fill="currentColor" d="M112 112c0 35.3-28.7 64-64 64l0 160c35.3 0 64 28.7 64 64l288 0c0-35.3 28.7-64 64-64l0-160c-35.3 0-64-28.7-64-64l-288 0zM0 128C0 92.7 28.7 64 64 64l384 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 448c-35.3 0-64-28.7-64-64L0 128zm256 16a112 112 0 1 1 0 224 112 112 0 1 1 0-224zm-16 44c-11 0-20 9-20 20 0 9.7 6.9 17.7 16 19.6l0 48.4-4 0c-11 0-20 9-20 20s9 20 20 20l48 0c11 0 20-9 20-20s-9-20-20-20l-4 0 0-68c0-11-9-20-20-20l-16 0z"/>
+    </svg>Contas
+    </Link >
 
-<Link to="/calendar" className='pages'><svg className='cal' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"  width="30" height="30">
-<path fill="currentColor" d="M128 0C110.3 0 96 14.3 96 32l0 32-32 0C28.7 64 0 92.7 0 128l0 48 448 0 0-48c0-35.3-28.7-64-64-64l-32 0 0-32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 32-128 0 0-32c0-17.7-14.3-32-32-32zM0 224L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-192-448 0z"/>
-</svg>Calendario</Link >
 
 
-<div className='pages'><svg className='assinatura'xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"  width="30" height="30"
-><path fill="currentColor" d="M311.4 32l8.6 0c35.3 0 64 28.7 64 64l0 352c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 96C0 60.7 28.7 32 64 32l8.6 0C83.6 12.9 104.3 0 128 0L256 0c23.7 0 44.4 12.9 55.4 32zM248 112c13.3 0 24-10.7 24-24s-10.7-24-24-24L136 64c-13.3 0-24 10.7-24 24s10.7 24 24 24l112 0zM128 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm32 0c0 13.3 10.7 24 24 24l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-112 0c-13.3 0-24 10.7-24 24zm0 128c0 13.3 10.7 24 24 24l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-112 0c-13.3 0-24 10.7-24 24zM96 416a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/>
-</svg>Assinaturas</div>
+    <Link to="/calendar" className='pages'><svg className='cal' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"  width="30" height="30">
+    <path fill="currentColor" d="M128 0C110.3 0 96 14.3 96 32l0 32-32 0C28.7 64 0 92.7 0 128l0 48 448 0 0-48c0-35.3-28.7-64-64-64l-32 0 0-32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 32-128 0 0-32c0-17.7-14.3-32-32-32zM0 224L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-192-448 0z"/>
+    </svg>Calendario
+    </Link >
 
-    <div className='pages'><svg className='rel' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="30" height="30">
-  <path fill="currentColor" d="M32 32c17.7 0 32 14.3 32 32l0 336c0 8.8 7.2 16 16 16l400 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L80 480c-44.2 0-80-35.8-80-80L0 64C0 46.3 14.3 32 32 32zM144 224c17.7 0 32 14.3 32 32l0 64c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32zm144-64l0 160c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-160c0-17.7 14.3-32 32-32s32 14.3 32 32zm80 32c17.7 0 32 14.3 32 32l0 96c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-96c0-17.7 14.3-32 32-32zM512 96l0 224c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-224c0-17.7 14.3-32 32-32s32 14.3 32 32z" />
-</svg>Relatorios</div>
+
+    <div className='pages'>
+    <svg className='assinatura'xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"  width="30" height="30"
+    ><path fill="currentColor" d="M311.4 32l8.6 0c35.3 0 64 28.7 64 64l0 352c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 96C0 60.7 28.7 32 64 32l8.6 0C83.6 12.9 104.3 0 128 0L256 0c23.7 0 44.4 12.9 55.4 32zM248 112c13.3 0 24-10.7 24-24s-10.7-24-24-24L136 64c-13.3 0-24 10.7-24 24s10.7 24 24 24l112 0zM128 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm32 0c0 13.3 10.7 24 24 24l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-112 0c-13.3 0-24 10.7-24 24zm0 128c0 13.3 10.7 24 24 24l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-112 0c-13.3 0-24 10.7-24 24zM96 416a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/>
+    </svg>Assinaturas
+    </div>
+
+    <div className='pages'>
+    <svg className='rel' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="30" height="30">
+    <path fill="currentColor" d="M32 32c17.7 0 32 14.3 32 32l0 336c0 8.8 7.2 16 16 16l400 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L80 480c-44.2 0-80-35.8-80-80L0 64C0 46.3 14.3 32 32 32zM144 224c17.7 0 32 14.3 32 32l0 64c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32zm144-64l0 160c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-160c0-17.7 14.3-32 32-32s32 14.3 32 32zm80 32c17.7 0 32 14.3 32 32l0 96c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-96c0-17.7 14.3-32 32-32zM512 96l0 224c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-224c0-17.7 14.3-32 32-32s32 14.3 32 32z" />
+    </svg>Relatorios</div>
 
     </div>
 
 
+    {token && (
+      <div className='profileWrapper'>
 
-  <div className='profileWrapper'>
-
-  <div
-    className='profile'
-    onClick={() => setMenuOpen(!menuOpen)}
+          <div
+            className='profile'
+            onClick={() => setMenuOpen(!menuOpen)}
   >
-    <svg className='user' xmlns="http://w3.org" viewBox="0 0 448 512" width="60" height="60">
-      <path fill="rgb(255, 255, 255)" d="M224 248a120 120 0 1 0 0-240 120 120 0 1 0 0 240zm-29.7 56C95.8 304 16 383.8 16 482.3 16 498.7 29.3 512 45.7 512l356.6 0c16.4 0 29.7-13.3 29.7-29.7 0-98.5-79.8-178.3-178.3-178.3l-59.4 0z"/>
-    </svg>
-  </div>
+                <svg className='user' xmlns="http://w3.org" viewBox="0 0 448 512" width="60" height="60">
+                  <path fill="rgb(255, 255, 255)" d="M224 248a120 120 0 1 0 0-240 120 120 0 1 0 0 240zm-29.7 56C95.8 304 16 383.8 16 482.3 16 498.7 29.3 512 45.7 512l356.6 0c16.4 0 29.7-13.3 29.7-29.7 0-98.5-79.8-178.3-178.3-178.3l-59.4 0z"/>
+                </svg>
+          </div>
 
-{menuOpen && (
-  <div className='profileMenu'>
+    {menuOpen && (
+      <div className='profileMenu'>
 
-    <button
-      className='logoutBtn'
-      onClick={() => {
-        logout()
-        setMenuOpen(false)
-      }}
-    >
+          <button
+          className='logoutBtn'
+         onClick={() => {
+
+    const confirmLogout = window.confirm("Tem certeza que deseja sair?")
+
+        if (confirmLogout) {
+          logout()
+          setMenuOpen(false)
+        }
+
+    }}>
       Logout
-    </button>
+      </button>
 
-  </div>
-)}
+      </div>
+    )}
 
 </div>
+)}
     </div>
   )
 }
